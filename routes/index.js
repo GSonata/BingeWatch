eaffc5058be317378ed1b568a6eb41538e393dbe
Jose Prados Morente <joseprados1998@gmail.com>
2024-10-18 12:13:51 +0200

    iconos?

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect("home.html");
 });
 
+router.get('/', function(req, res, next) {
+  res.redirect("home.html");
+});
 module.exports = router;
